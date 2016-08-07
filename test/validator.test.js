14d13ebd365a590280576fbf3a874582bc3bd414
Florian Edelmaier <florian.edelmaier@gmail.com>
2016-08-07 12:59:35 +0200

    do not throw exceptions if property not defined but add new validation error

--- test/validator.test.js
+++ test/validator.test.js
@@ -44,15 +44,6 @@ describe('Validator', () => {
         //     v.check(validations.isMandatory, str => str === 'xyz')(test, 'test', 'test is required');
         //     expect(v.hasErrors()).toEqual(false);
         // });
-        it('should throw exception if marked as mandatory and does not exist', () => {
-            const req = { params: {} };
-            const v = new Validator();
-            let error;
-            try {
-                v.check(validations.isMandatory)(req.params, 'test', 'test');
-            } catch(err) { error = err; }
-            expect(error).toEqual(new Error('Given object is not defined'));
-        });
         it('should omit validators if check is marked as optional', () => {
             const req = { params: {} };
             const v = new Validator();
@@ -65,6 +56,22 @@ describe('Validator', () => {
             v.check(validations.isOptional, validations.isEmail)(req.params, 'email', 'email should be valid');
             expect(v.hasErrors()).toEqual(true);
             expect(v.errors[0].message).toEqual('email should be valid');
+        });
+        it('should omit following validations if isMandatory already failed', () => {
+            const req = { body: { test: 'xyz' } };
+            const v = new Validator();
+            v.check(validations.isMandatory, validations.isEmail)(req.body, 'email', 'email should be valid');
+            expect(v.hasErrors()).toEqual(true);
+            expect(v.errors.length).toEqual(1);
+            expect(v.errors[0].message).toEqual('Given property is not defined');
+        });
+        it('should create "not defined" error if validation checks not existing property and isMandatory not enforced', () => {
+            const req = { body: { test: 'xyz' } };
+            const v = new Validator();
+            v.check(validations.isEmail)(req.body, 'email', 'email should be valid');
+            expect(v.hasErrors()).toEqual(true);
+            expect(v.errors.length).toEqual(1);
+            expect(v.errors[0].message).toEqual('Given property is not defined');
         });
         it('should create no error if isIn validation is fullfilled', () => {
             const req = { body: { type: 'a' } };
